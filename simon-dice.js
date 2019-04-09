c6b5c08cdc86d45e8f1748bb4c4ef558500be7e3
JosueOb <josuericardocando@hotmail.com>
2019-04-09 00:18:39 -0500

    Generando una secuencia de números

--- simon-dice.js
+++ simon-dice.js
@@ -13,6 +13,7 @@ class Juego{
 
     constructor(){
         this.inicializar()
+        this.generarSecuencia()
     }
     // Métodos
     inicializar(){
@@ -20,11 +21,33 @@ class Juego{
         // classList, agrega a las clasess CSS del btnEmpezar
         // una nueva clase CSS
         btnEmpezar.classList.add('hide')
+        this.nivel = 1
+        this.colores = {
+            // en el caso de asignar a un atributo el valor que se tiene en una variable
+            // con el mismo nombre del atributo, solo basta indicarle el mismo nombre
+            celeste: celeste,
+            violeta, //es lo mismo que celeste: celestes (referente al boton)
+            naranja,
+            verde
+        }
+    }
+    generarSecuencia(){
+        // se puede crear un atributo nuevo, guardandolo en el objeto Juego
+        // se puede generar un array con la palabra reservada new Array, especificando en su constructor
+        // cuantos elementos se desea que tengá el array. Después se llama a la función fill, la cual recibe 
+        // un valor que llenara a todo el arreglo de dicho valor ingresado. Además, es importante llamar a esta función
+        // fill ya que al utilizar la función map en este array, no funcionará debido el array no tiene elementos definidos y con
+        // un valor. Para redondear un valor para bajo, se utiliza Math.floor
+        this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
     }
 }
 
 function empezarJuego(){
     // alert("El juego va a comenzar")
     // se crea un objeto de la clase juego
-    var juego = new Juego()
+    // var juego = new Juego()
+
+    // A modo de prueba, se pone la variable juego dentro de windows para poder debuggear, es decir, observar en consola
+    // que es lo que esta pasando y acceder a la secuencia generada.
+    window.juego = new Juego()
 }
