9e44ca8b6030878133cd2057e0e52e2d99a7f9f7
JosueOb <josuericardocando@hotmail.com>
2019-04-11 18:25:02 -0500

    Agregando la verificación del color elegido

--- simon-dice.js
+++ simon-dice.js
@@ -7,6 +7,7 @@ const celeste = document.getElementById('celeste')
 const violeta = document.getElementById('violeta')
 const naranja = document.getElementById('naranja')
 const verde = document.getElementById('verde')
+const ULTIMO_NIVEL = 10
 
 // La clase juego tendrá toda la lógia del juego
 class Juego{
@@ -14,13 +15,14 @@ class Juego{
     constructor(){
         this.inicializar()
         this.generarSecuencia()
-        this.siguienteNivel()
+        setTimeout(this.siguienteNivel, 500)
     }
     // Métodos
     inicializar(){
         //cada vez que se utilice elegirJuego, va estar atado al this del juego
         // de este modo this es el juego y no cada uno de los botones dentro de la elegirColor()
         this.elegirColor = this.elegirColor.bind(this)
+        this.siguienteNivel = this.siguienteNivel.bind(this)//esta atado al juego
         // se oculta el boton de empezar
         // classList, agrega a las clasess CSS del btnEmpezar
         // una nueva clase CSS
@@ -42,9 +44,10 @@ class Juego{
         // un valor que llenara a todo el arreglo de dicho valor ingresado. Además, es importante llamar a esta función
         // fill ya que al utilizar la función map en este array, no funcionará debido el array no tiene elementos definidos y con
         // un valor. Para redondear un valor para bajo, se utiliza Math.floor
-        this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
+        this.secuencia = new Array(ULTIMO_NIVEL).fill(0).map(n => Math.floor(Math.random() * 4))
     }
     siguienteNivel(){
+        this.subnivel = 0
         this.iluminarSecuencia()
         this.agregarEventosClick()
     }
@@ -58,6 +61,18 @@ class Juego{
                 return 'naranja'
             case 3:
                 return 'verde'
+        }
+    }
+    transformarColorANumero(numero){
+        switch(numero){
+            case 'celeste':
+                return 0//no se pone break, ya que el break no se podra ejecutar si se tiene un return
+            case 'violeta':
+                return 1
+            case 'naranja':
+                return 2
+            case 'verde':
+                return 3
         }
     }
     iluminarSecuencia(){
@@ -91,8 +106,8 @@ class Juego{
         // lo que realiza es este metodo es agregar a cada color el evento click, y decirle al navegador
         // que función debe ejecutar una vez que se realice ese evento click, esta función se ejecuta asincronamente
         // Para agregar un manejador de un evento se utiliza addEventListener que recibe el evento y la función que se debe ejecutar
-        var _this = this
-        var self = this//para no perder la referencia de quien es this
+        // var _this = this
+        // var self = this//para no perder la referencia de quien es this
         // this.colores.celeste.addEventListener('click', this.elegirColor.bind(self))//para evitar utilizar bind en este método y especificar que elegirJuego
         // este atado a this (de la clase juego) se lo especifica en inicializar()
         this.colores.celeste.addEventListener('click', this.elegirColor)
@@ -100,15 +115,50 @@ class Juego{
         this.colores.naranja.addEventListener('click', this.elegirColor)
         this.colores.verde.addEventListener('click', this.elegirColor)
     }
+    eliminarEventosClick(){
+        this.colores.celeste.removeEventListener('click', this.elegirColor)
+        this.colores.violeta.removeEventListener('click', this.elegirColor)
+        this.colores.naranja.removeEventListener('click', this.elegirColor)
+        this.colores.verde.removeEventListener('click', this.elegirColor)
+    }
     // cuando se agregan manejadores de eventos o escuchadores de evento
     // los métodos que se llaman, se llaman con un parametro que es ev
     elegirColor(ev){
         // console.log(ev)
         // JavaScript va a perder el contexto cuando se llame a elegirColor
         // Es muy importante de recordar quien es el this, dentro de cada una de las funciones
-        console.log(this)//this en este caso es el botón al que se esta llamando, this dentro dentro de ese método
+        // console.log(this)//this en este caso es el botón al que se esta llamando, this dentro dentro de ese método
         // pero si de desea que this siga siendo un objeto juego, para ello se debe cambiar quien es this, utilizando
         // baind (enlazar), especificando quien es this.
+
+        // console.log(ev)//dentro del evento mouse se tiene un atributo target que tiene el boton que fue tocada, y dento de este se tiene un atributo dataset que tiene el color del boton
+        // este atributo data-color se tiene en cada uno de las etiquetas div de cada color
+
+        const nombreColor = ev.target.dataset.color
+        const numeroColor = this.transformarColorANumero(nombreColor)
+        this.iluminarColor(nombreColor)
+        // se compara el numero del color, con la secuencia pero en la posicion del subnivel del que se encuentra
+        if(numeroColor === this.secuencia[this.subnivel]){
+            this.subnivel++//se incrementa
+            if(this.subnivel === this.nivel){
+                //pasa de nivel
+                this.nivel++
+                //se eleminan los elementos de click
+                this.eliminarEventosClick()
+                if(this.nivel === (ULTIMO_NIVEL + 1)){
+                    // ganó
+                }else{
+                    // setTimeout(this.siguienteNivel.bind(this), 2000)//se utiliza una referencia a la función y el tiempo
+                    // en este caso this es window, ya que setTimeOut delega en el navegador la ejecución de la función
+                    setTimeout(this.siguienteNivel, 1500)//se utiliza bind en inicializar()
+
+                    
+                }
+            }
+        }else{
+            // perdió
+        }
+
     }
 }
 
